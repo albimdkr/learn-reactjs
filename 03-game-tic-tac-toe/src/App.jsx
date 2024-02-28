8a7479100f9cc9a0a343f1f855a30b777914dcc4
albimdkr <albimudakar04@gmail.com>
2024-02-28 21:08:39 +0700

    feat: add fase 2 logic event for determine X or O

--- 03-game-tic-tac-toe/src/App.jsx
+++ 03-game-tic-tac-toe/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
 
@@ -22,31 +23,43 @@ function App() {
   }
 
   // Square 
-  function Square(){
-    // State
-    const [value, setValue] = useState('');
-
-    function handleClickX (){
-      setValue('X');
-    }
-
+  function Square({ value, onSquareClick }){
     return (
-      <button className='square' onClick={handleClickX}>{value}</button>
+      <button className='square' onClick={onSquareClick}>{value}</button>
     )
   }
 
   function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    // Changes Feature between X and O
+    // Bolean If true X else O
+    const [xIsNext, setXIsNext] = useState(true);
+
+    function handleClick(i){
+      // Block event when the button !empty
+      if (squares[i]) return;
+
+      const nextSquares = squares.slice();
+
+      // Condition to check true or false
+      nextSquares[i] = xIsNext ? 'X' : 'O';
+
+      setSquares(nextSquares);
+      setXIsNext(!xIsNext);
+    }
+
     return (
       <div className="board">
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     );
   }
